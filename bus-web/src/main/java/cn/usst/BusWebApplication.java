a374cfe15f7e6c693ae7a62cb3873b9f35519d9d
meko <lfywh12@163.com>
2019-01-17 12:45:30 +0800

    bus-demo

---
+++ bus-web/src/main/java/cn/usst/BusWebApplication.java
@@ -0,0 +1,14 @@
+package cn.usst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusWebApplication.class, args);
+	}
+
+}
+
